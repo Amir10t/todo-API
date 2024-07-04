4a81e7e494cde6a0cf8ff3ee6c947ba0ac53b004
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-07-04 18:15:20 +0330

    Create Todo Model % Do the Migratinos

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    priority = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+
+    def __str__(self):
+        return f"{self.title} -> {self.is_done}"
+
+    class Meta:
+        db_table = 'todo'
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todos'
