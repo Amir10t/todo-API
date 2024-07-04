f7e74664dffb6d9fdd60535373a797127f6d88fd
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-07-04 18:24:53 +0330

    Set The Admin Setting

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TodoModel
 
 # Register your models here.
+
+@admin.register(TodoModel)
+class TodoModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'is_done')
