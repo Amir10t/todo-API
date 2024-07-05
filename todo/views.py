ae22c5477a6268523d655ea201916d2f6f0ca8a9
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-07-05 11:34:04 +0330

    Create User APiViews

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
-from todo.models import TodoModel
-from todo.serializer import TodoSerializer
+from todo.models import TodoModel, User
+from todo.serializer import TodoSerializer, UserSerializer
 
 
 # Create your views here.
-
+#region TODOS APIVIEW
 class TodoListAPIView(generics.ListCreateAPIView):
     queryset = TodoModel.objects.order_by('priority').all()
     serializer_class = TodoSerializer
@@ -13,3 +13,10 @@ class TodoListAPIView(generics.ListCreateAPIView):
 class TodoDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TodoModel.objects.order_by('priority').all()
     serializer_class = TodoSerializer
+#endregion
+
+#region USER APIVIEW
+class UserListAPIView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+#endregion
