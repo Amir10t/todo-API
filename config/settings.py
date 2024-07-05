96c8c053cd788ff8a3e3045d1d45842fca3fa51e
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-07-05 11:40:05 +0330

    Add TokenAuthentication

--- config/settings.py
+++ config/settings.py
@@ -129,5 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Rest Freamwork Settings
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ['rest_framework.authentication.TokenAuthentication'],
+    "DEFAULT_PERMISSION_CLASSES": ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
