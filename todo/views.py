4d4d279e65f0b4fb3ff46543e4e863aa126d9950
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-07-05 11:20:18 +0330

    Create APIView Calsses

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from todo.models import TodoModel
+from todo.serializer import TodoSerializer
+
 
 # Create your views here.
+
+class TodoListAPIView(generics.ListCreateAPIView):
+    queryset = TodoModel.objects.order_by('priority').all()
+    serializer_class = TodoSerializer
+
+class TodoDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TodoModel.objects.order_by('priority').all()
+    serializer_class = TodoSerializer
