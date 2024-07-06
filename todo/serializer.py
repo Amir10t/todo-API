ef525715f8ef8129e44d853a1490fdf847f120d0
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-07-06 17:44:47 +0330

    Add validate priority

--- todo/serializer.py
+++ todo/serializer.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from todo.models import TodoModel, User
 
 
 class TodoSerializer(ModelSerializer):
+    def validate_priority(self, priority):
+        if priority <= 0 or priority > 4:
+            raise serializers.ValidationError("It must be between 1 and 4")
+        return priority
+
     class Meta:
         model = TodoModel
         fields = '__all__'
