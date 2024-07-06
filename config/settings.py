c52f6d6057a097e9cbb1cb3771a532c08e66235d
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-07-06 17:37:47 +0330

    Add Basic Authentication

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Rest Freamwork Settings
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": ['rest_framework.authentication.TokenAuthentication'],
+    "DEFAULT_AUTHENTICATION_CLASSES": ['rest_framework.authentication.TokenAuthentication', 'rest_framework.authentication.BasicAuthentication'],
     "DEFAULT_PERMISSION_CLASSES": ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
